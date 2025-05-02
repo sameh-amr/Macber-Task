3275d1e5c03c4968ce27a5d5e69b26e8001614a5
sameh-amr <samehamrsw@gmail.com>
2025-05-03 02:07:59 +0300

    added the required types for our app

---
+++ client/src/types/AuthContextDto.ts
@@ -0,0 +1,3 @@
+export interface AuthContextType {
+  token: string | null;
+}
---
+++ client/src/types/FeedbackDto.ts
@@ -0,0 +1,9 @@
+export interface FeedbackDto {
+    id?: string;
+    name: string;
+    email: string;
+    message: string;
+    rating: number;
+    createdAt?: string;
+  }
+  
