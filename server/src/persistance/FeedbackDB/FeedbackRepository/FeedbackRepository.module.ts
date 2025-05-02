cfddfc93d0d774c9872350b2b4e9d8572f9f5e6a
sameh-amr <samehamrsw@gmail.com>
2025-05-03 00:47:15 +0300

    finalized some code cleaning and enhancments

--- server/src/persistance/FeedbackDB/FeedbackRepository/FeedbackRepository.module.ts
+++ server/src/persistance/FeedbackDB/FeedbackRepository/FeedbackRepository.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { IFeedbackRepository } from 'src/application/Interfaces/IFeedbackRepository';
 import { FeedbackRepository } from './FeedbackRepository.service';
-import { DatabaseModule } from 'src/persistance/DBConfig/DbConfig.module';
 
 @Module({
-  imports: [DatabaseModule],
   providers: [{ useClass: FeedbackRepository, provide: IFeedbackRepository }],
   exports: [IFeedbackRepository],
 })
