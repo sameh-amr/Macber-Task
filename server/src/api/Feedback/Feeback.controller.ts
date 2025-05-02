1a2ddfe2cde9c7f06845680f24600ca237f68cc7
sameh-amr <samehamrsw@gmail.com>
2025-05-03 01:36:36 +0300

    added the protected route to be used in our backend servers

--- server/src/api/Feedback/Feeback.controller.ts
+++ server/src/api/Feedback/Feeback.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { FeedbackEntity } from 'src/domain/feedback.entity';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import InsertFeedbackCommand from 'src/application/Feedback/InsertFeedback/InsertFeedbackCommand.service';
 import GetAllFeedbacksCommand from 'src/application/Feedback/GetAllFeedbacks/GetAllFeedbacksCommand.service';
 import GetFeedbackByIdCommand from 'src/application/Feedback/GetFeedbackById/GetFeedbackByIdCommand.service';
@@ -17,11 +19,12 @@ class FeedbackController {
   }
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   async getAllFeedbacks() {
     return await this.getAllFeedbacksCommand.execute();
   }
-
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   async getFeedbackById(@Param('id') id: string) {
     console.log(id)
     return await this.getFeedbackCommandById.execute(id);
